2a96ec36fc60854a2c275f70fd07c8ecb0349124
mahadevsarkar991-web <mahadevsarkar991@gmail.com>
2025-11-09 15:20:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const topBtn = document.getElementById("topBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      topBtn.classList.add("show");
+    } else {
+      topBtn.classList.remove("show");
+    }
+  });
+
+  topBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+  
